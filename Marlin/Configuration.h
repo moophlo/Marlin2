c0b5deb97032894cd434bfce841de0b46bfc5eed
muflo <andrea.odorisio@gmail.com>
2020-10-07 20:03:45 +0200

    Marlin2.0.7_Ender5_SKR_v1.4_TURBO_v0.8-Rollback_Microstepping_to_stock

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -773,8 +773,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.84, 80.34, 398.01, 430.03}
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 319.36, 321.36, 6368.16, 6880.48}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.84, 80.34, 398.01, 430.03}
 
 
 /**
