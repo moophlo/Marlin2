edbf5946e72c9248b236df5d3501ab98cc257792
muflo <andrea.odorisio@gmail.com>
2020-10-09 19:12:47 +0200

    Marlin2.0.7_Ender5_SKR_v1.4_TURBO_v0.11-disabledhomeXbeforeY-disabledHomingReliability

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -640,7 +640,7 @@
 //#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (mm) Backoff from endstops after homing
 
 //#define QUICK_HOME                          // If G28 contains XY do a diagonal move first
-#define HOME_Y_BEFORE_X                     // If G28 contains XY home Y before X
+//#define HOME_Y_BEFORE_X                     // If G28 contains XY home Y before X
 //#define CODEPENDENT_XY_HOMING               // If X/Y can't home without homing Y/X first
 
 // @section bltouch
@@ -2534,7 +2534,7 @@
     //#define Z3_STALL_SENSITIVITY Z_STALL_SENSITIVITY
     //#define Z4_STALL_SENSITIVITY Z_STALL_SENSITIVITY
     #define SPI_ENDSTOPS              // TMC2130 only
-    #define IMPROVE_HOMING_RELIABILITY
+    //#define IMPROVE_HOMING_RELIABILITY
   #endif
 
   /**
