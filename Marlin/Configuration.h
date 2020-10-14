aa6e9f744dd39bce96c77d7c62a2c33953932921
muflo <andrea.odorisio@gmail.com>
2020-10-14 17:08:06 +0200

    Marlin2.0.7_Ender5_SKR_v1.4_TURBO_v0.22-microstep_e_32

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -773,8 +773,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 638.72, 642.72, 399.40, 6880.48}
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 638.72, 642.72, 399.40, 430.03}
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 638.72, 642.72, 399.40, 6880.48}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 638.72, 642.72, 399.40, 860.06}
 
 
 
